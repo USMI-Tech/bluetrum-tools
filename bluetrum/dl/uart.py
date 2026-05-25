f02c09a875bad101f7c450cf96e196e07c358f2d
USMI-Tech <121731611+USMI-Tech@users.noreply.github.com>
2026-05-25 18:15:10 +0200

    Refactor UART for CRWN

--- bluetrum/dl/uart.py
+++ bluetrum/dl/uart.py
@@ -1,26 +1,27 @@
 from bluetrum.crc import ab_crc16
-import time
+
 
 class UARTDownload:
     # special tokens
     SYNC_TOKEN  = b'\xA5\x96\x87\x5A'   # Sync token
     SYNC_RESP   = b'\x5A\x69\x78\xA5'   # Sync response
-    RESET_TOKEN = b'\xF5\xA0'           # Communication Reset token (sending sync token next to it resets the chip instead)
+    RESET_TOKEN = b'\xF5\xA0'           # Communication Reset token
 
     # response tokens
     RESP_ACK     = 0x1E   # Data packet has been accepted
     RESP_NAK     = 0x2D   # No data available (receive); No space for data (send)
-    RESP_NYET    = 0x3C   # Data packet accepted but there is no more space for another one. (not processed yet)
+    RESP_NYET    = 0x3C   # Data packet accepted but there is no more space for another one.
 
     # request tokens
-    DATA_TOKEN   = 0x4B   # Data token. ACK: all right, NYET: previous one hasn't been received yet, NAK: could not receive
-    DATA_REQUEST = 0xB4   # Request for data. DATA: there it is, NAK: not available
-    PING_TOKEN   = 0xC3   # Can we send more data? ACK: yes, NAK: no
+    DATA_TOKEN   = 0x4B   # Data token.
+    DATA_REQUEST = 0xB4   # Request for data.
+    PING_TOKEN   = 0xC3   # Can we send more data?
 
     #------------------------------------------
 
-    def __init__(self, port):
+    def __init__(self, port, has_echo=True):
         self.port = port
+        self.has_echo = has_echo
         self.comms_reset()
 
     def port_read(self, size):
@@ -31,12 +32,9 @@ class UARTDownload:
 
     def port_write(self, data):
         self.port.write(data)
-        # consume the echo back
-        echo = self.port.read(len(data))
-        #if len(echo) < len(data):
-        #    raise TimeoutError('Did not receive the echo back')
-        #if echo != data:
-        #    raise ValueError('The echo has been corrupted')
+        if self.has_echo:
+            # consume the echo back (AB560X/PRAO echoes TX bytes)
+            self.port.read(len(data))
 
     def comms_reset(self):
         self.counter = 0
@@ -64,7 +62,7 @@ class UARTDownload:
         recv = self.port_read(2)
         # check the counter value
         if recv[1] != self.counter:
-            return ValueError(f'Mismatch in the counter value of a received token ({recv[1]}) from expected ({self.counter})')
+            raise ValueError(f'Mismatch in counter value ({recv[1]}) from expected ({self.counter})')
         # return the received token value
         return recv[0]
 
@@ -91,13 +89,10 @@ class UARTDownload:
 
         do_ping = self.ping_before_send
 
-        # TODO: time out, maybe?
         while True:
             if do_ping:
-                # send a PING token
                 packet = self._make_token_packet(UARTDownload.PING_TOKEN)
             else:
-                # send a DATA packet
                 packet = self._make_token_packet(UARTDownload.DATA_TOKEN) + data
 
             tries = 0
@@ -107,7 +102,6 @@ class UARTDownload:
                 try:
                     resp = self._recv_token_packet()
                 except TimeoutError:
-                    # maybe a reception failure or a CRC error.
                     if tries > 10:
                         raise TimeoutError("Could not send a data packet.")
                     tries += 1
@@ -116,44 +110,34 @@ class UARTDownload:
 
             if do_ping:
                 if resp == UARTDownload.RESP_ACK:
-                    # Now we can send data.
                     do_ping = False
                 elif resp == UARTDownload.RESP_NAK:
-                    # Not yet.
                     pass
                 else:
                     raise RuntimeError(f'Ping: Unexpected response token {resp:02X}')
             else:
                 if resp == UARTDownload.RESP_ACK:
-                    # All fine
                     self.ping_before_send = False
                     return
                 elif resp == UARTDownload.RESP_NYET:
-                    # Received fine, but the previous data block hasn't been processed yet.
-                    self.ping_before_send = True  # better to ping it next time.
+                    self.ping_before_send = True
                     return
                 elif resp == UARTDownload.RESP_NAK:
-                    # Failed, perhaps the chip is busy.
-                    do_ping = True  # start pinging.
+                    do_ping = True
                 else:
                     raise RuntimeError(f'Tx: Unexpected response token {resp:02X}')
 
     def recv_packet(self):
-        # the request packet needs to be sent with the same counter value
-        #  in case we need to re-request data in case of a CRC failure etc.
-        #  otherwise the chip assumes the data was received ok
         request = self._make_token_packet(UARTDownload.DATA_REQUEST)
 
         tries = 0
 
-        # TODO: a timeout?
         while True:
             self.port_write(request)
 
             try:
                 resp = self._recv_token_packet()
             except TimeoutError:
-                # maybe chip didn't receive the request
                 if tries > 10:
                     raise TimeoutError("Could not request a data packet.")
                 tries += 1
@@ -162,18 +146,13 @@ class UARTDownload:
                 tries = 0
 
             if resp == UARTDownload.DATA_TOKEN:
-                # Here's the data
                 try:
-                    # receive the data packet
                     data = self._recv_data_payload()
-                except:
-                    # something failed, ask for data again.
+                except Exception:
                     pass
                 else:
-                    # successful reception
                     return data
             elif resp == UARTDownload.RESP_NAK:
-                # Failed, chip does not have any data block yet.
                 continue
             else:
                 raise RuntimeError(f'Rx: Unexpected response token {resp:02X}')
